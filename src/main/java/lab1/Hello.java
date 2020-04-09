ca182e629e3418be64e3cbbd97c32ec023ef9ba4
Andrew Dunn <dunna21@wwu.edu>
2020-04-09 13:17:12 -0700

    Hello now prints with command line arg

--- src/main/java/lab1/Hello.java
+++ src/main/java/lab1/Hello.java
@@ -2,6 +2,13 @@ package lab1;
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    String input = "world";
+    try{
+      input = args[0];
+    }
+    catch(Exception e){
+      System.out.println("Uh oh... \t No args given!");
+    }
+    System.out.println("Hello, " + input + "!");
   }
 }
